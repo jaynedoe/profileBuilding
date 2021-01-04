60e59a34420111225b3594dddd1db1a33049888c
jaynedoe <jaedoyne@hotmail.com>
2021-01-04 18:52:14 +1100

    initial commit

---
+++ server.js
@@ -0,0 +1,153 @@
+//BUILD SERVER
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+//const findOrCreate = require("mongoose-findorcreate");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  belief: String,
+  firstName: {type: String, default: "Friend"}
+});
+
+userSchema.plugin(passportLocalMongoose);
+//userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+app.get("/profile", function (req, res) {
+   
+    if (req.isAuthenticated()) {
+    User.findById(req.user.id, function (err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+            res.render("profile", { foundUser: foundUser });
+        }
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/edit", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("edit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        res.redirect("/login");
+      }
+    }
+  );
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/profile");
+      });
+    }
+  });
+});
+
+app.post("/edit", function (req, res) {
+  const newBelief = req.body.belief;
+  const firstName = req.body.firstName;
+
+  User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.firstName = firstName;
+        foundUser.belief = newBelief;
+        foundUser.save(function () {
+          res.redirect("/profile");
+        });
+      }
+    }
+  });
+
+  //extract details of profile edits and send to profile page.
+});
+
+app.listen(3000, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Server listening on port 3000.");
+  }
+});
